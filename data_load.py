b0e923f722d33fe360653ba2b1776ebd97f6f65c
aliskadmitr <alisadmitr@bk.ru>
2024-11-18 12:29:06 +0300

    Add in ImageDataset classs default transformatiob of data

--- data_load.py
+++ data_load.py
@@ -4,6 +4,7 @@ import torch
 from PIL import Image
 import numpy as np
 from torch.utils.data import Dataset
+from torchvision import transforms
 
 
 
@@ -62,5 +63,13 @@ class ImageDataset(Dataset):
 
         if self.transform:
             image = self.transform(image)
+
+        else:  
+            default_transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+            image = default_transform(image)
 
         return image, torch.tensor(label).long()
