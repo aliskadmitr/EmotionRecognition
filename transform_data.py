88302a8a65c2f098a0e373f3750f11df4875c5d8
aliskadmitr <alisadmitr@bk.ru>
2024-11-30 20:04:08 +0300

    crate class ImageTransformation to transform data into required form

--- transform_data.py
+++ transform_data.py
@@ -1,57 +1,28 @@
-import random
-from torchvision import transforms
-from torch.utils.data import DataLoader
-from data_load import ImageDataset
+from torchvision.transforms import (
+    Compose, Resize, RandomRotation, RandomAdjustSharpness,
+    RandomHorizontalFlip, Normalize, ToTensor
+)
+from transformers import AutoImageProcessor
 
-def augment_small_classes(dataset, target_classes, target_size, augmentation_transform):
-    augmented_images = []
-    augmented_labels = []
+class ImageTransformations:
+    def __init__(self, model_checkpoint: str):
+        self.image_processor = AutoImageProcessor.from_pretrained(model_checkpoint)
+        self.image_mean, self.image_std = self.image_processor.image_mean, self.image_processor.image_std
+        self.size = self.image_processor.size["height"]
 
-    for class_label in target_classes:
-        class_indices = [i for i, label in enumerate(dataset.labels) if label == class_label]
-        
-        while len(class_indices) < target_size:
-            idx = random.choice(class_indices)
-            image, label = dataset[idx]
-            augmented_image = augmentation_transform(image)
-            augmented_images.append(augmented_image)
-            augmented_labels.append(label)
-            class_indices.append(idx)
+    def get_train_transforms(self):
+        return Compose([
+            Resize((self.size, self.size)),
+            RandomRotation(90),
+            RandomAdjustSharpness(2),
+            RandomHorizontalFlip(0.5),
+            ToTensor(),
+            Normalize(mean=self.image_mean, std=self.image_std)
+        ])
 
-    return augmented_images, augmented_labels
-
-def prepare_dataloaders(train_dataset, val_dataset, test_dataset, batch_size, augmentation_transform, target_classes, target_size):
-
-
-    augmented_images, augmented_labels = augment_small_classes(
-        train_dataset, target_classes, target_size, augmentation_transform
-    )
-
-    all_images = train_dataset.image_paths + augmented_images
-    all_labels = list(train_dataset.labels) + augmented_labels
-
-    train_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    test_val_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    train_aug_dataset = ImageDataset(
-        data_dir=None, folder=None, images=all_images, labels=all_labels, transform=train_transforms
-    )
-    val_dataset.transform = test_val_transforms
-    test_dataset.transform = test_val_transforms
-
-    train_loader = DataLoader(train_aug_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    return train_loader, val_loader, test_loader
+    def get_val_transforms(self):
+        return Compose([
+            Resize((self.size, self.size)),
+            ToTensor(),
+            Normalize(mean=self.image_mean, std=self.image_std)
+        ])
